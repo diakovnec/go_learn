cdcd7fa7e7bf40dc5ffe8875392511050691059a
ydiakov <diakovnec@gmail.com>
2024-10-26 13:31:24 +0800

    Repear with Example

--- iterations/iterations.go
+++ iterations/iterations.go
@@ -1,10 +1,20 @@
+// package iteration
+
+// const repeatedCount = 5
+
+// func Repeat(character string) string {
+// 	var repeated string
+// 	for i := 0; i < repeatedCount; i++ {
+// 		repeated += character
+// 	}
+// 	return repeated
+// }
+
 package iteration
 
-const repeatedCount = 5
-
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < repeatedCount; i++ {
+func Repeat(character string, count int) string {
+	repeated := ""
+	for i := 0; i < count; i++ {
 		repeated += character
 	}
 	return repeated
