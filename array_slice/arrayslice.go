73849978eda33a04c93758023a4ae3cbfc9cfc31
ydiakov <diakovnec@gmail.com>
2024-10-27 10:50:25 +0800

    slices completed

--- array_slice/arrayslice.go
+++ array_slice/arrayslice.go
@@ -11,10 +11,20 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenghtOfNumbers := len(numbersToSum)
-	sums := make([]int, lenghtOfNumbers)
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
 	return sums
 }
